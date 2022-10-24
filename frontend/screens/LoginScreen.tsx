8d8dd31e9eeeb1f4675f31728f96a74499b05ae1
Mathieu Albin <mathieualbin@MacBook-Air-de-Mathieu.local>
2022-10-24 17:01:26 +0200

    login & inscription

--- frontend/screens/LoginScreen.tsx
+++ frontend/screens/LoginScreen.tsx
@@ -4,11 +4,18 @@ import { Platform, StyleSheet } from 'react-native';
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 import { RootStackScreenProps } from '../types';
+import { TextInput, Button } from "@react-native-material/core";
 
 export default function LoginScreen({ navigation }: RootStackScreenProps<'Login'>) {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Login</Text>
+      <TextInput variant="outlined" label="User" style={{ margin: 5 }} />
+      <TextInput variant="outlined" label="Password" secureTextEntry={true} style={{ margin: 5 }} />
+      <Button variant="outlined" title="Sign In"/>
+      <Text style={{color: 'blue', paddingTop: 50}} onPress={() => navigation.navigate("Inscription")}>
+        Créer un compte
+      </Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
     </View>
   );
@@ -23,6 +30,7 @@ const styles = StyleSheet.create({
   title: {
     fontSize: 20,
     fontWeight: 'bold',
+    marginBottom: 15
   },
   separator: {
     marginVertical: 30,
