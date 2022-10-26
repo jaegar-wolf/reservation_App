40b5ee98af5c484eacd4560f4e2153917007df4f
jaegar-wolf <mathiiew972@gmail.com>
2022-10-26 11:49:13 +0200

    login & inscription evolve

--- frontend/screens/InscriptionScreen.tsx
+++ frontend/screens/InscriptionScreen.tsx
@@ -1,23 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
-import { Platform, StyleSheet } from 'react-native';
+import { Platform, StyleSheet, ImageBackground } from 'react-native';
 
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 import { RootStackScreenProps } from '../types';
-import { TextInput, Button } from "@react-native-material/core";
+import { TextInput, Button } from 'react-native-paper';
 
 export default function InscriptionScreen({ navigation }: RootStackScreenProps<'Inscription'>) {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Inscription</Text>
-      <TextInput variant="outlined" label="First Name" style={{ margin: 5 }} />
-      <TextInput variant="outlined" label="Last Name" style={{ margin: 5 }} />
-      <TextInput variant="outlined" label="Email" style={{ margin: 10 }} />
-      <TextInput variant="outlined" label="Phone" style={{ margin: 10 }} />
-      <TextInput variant="outlined" label="Password" secureTextEntry={true} style={{ margin: 5 }} />
-      <TextInput variant="outlined" label="Confirmation Password" secureTextEntry={true} style={{ margin: 5 }} />
-      <Button variant="outlined" title="Sign In"/>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+      <ImageBackground source = {require("../assets/images/Background.jpg")} resizeMode="repeat" style={styles.image}>
+        <Text style={styles.title}>Registration</Text>
+        <TextInput mode="outlined" activeOutlineColor="green" label="First Name" style={{ margin: 5 }} />
+        <TextInput mode="outlined" activeOutlineColor="green" label="Last Name" style={{ margin: 5 }} />
+        <TextInput mode="outlined" activeOutlineColor="green" label="Email" style={{ margin: 10 }} />
+        <TextInput mode="outlined" activeOutlineColor="green" label="Phone" style={{ margin: 10 }} />
+        <TextInput mode="outlined" activeOutlineColor="green" label="Password" secureTextEntry={true} style={{ margin: 5 }} />
+        <TextInput mode="outlined" activeOutlineColor="green" label="Confirmation Password" secureTextEntry={true} style={{ margin: 5 }} />
+        <Button mode="outlined" textColor='green' style={{ marginTop: 10}} onPress={() => console.log('Pressed')}>
+          Create Account
+        </Button>
+        <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+      </ImageBackground>
     </View>
   );
 }
@@ -25,8 +29,7 @@ export default function InscriptionScreen({ navigation }: RootStackScreenProps<'
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    width: "100%"
   },
   title: {
     fontSize: 20,
@@ -38,4 +41,10 @@ const styles = StyleSheet.create({
     height: 1,
     width: '80%',
   },
+  image: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center"
+    
+  }
 });
