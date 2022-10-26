955e3bf5ce8889ae8b358db3069250cd939c9086
jaegar-wolf <mathiiew972@gmail.com>
2022-10-26 19:39:12 +0200

    modif userScreen

--- frontend/screens/UserScreen.tsx
+++ frontend/screens/UserScreen.tsx
@@ -4,33 +4,24 @@ import { Platform, StyleSheet, ImageBackground } from 'react-native';
 import { Avatar, Button, Card, Title, Paragraph, TextInput } from 'react-native-paper';
 
 export default function UserScreen({ navigation }: RootStackScreenProps<'User'>){
-    const LeftContent = (props:any) => <Avatar.Icon {...props} theme="green" icon="account" />
+    const LeftContent = (props:any) => <Avatar.Icon {...props} color="green" icon="account" />
 
     return (
         <View style={styles.container}>
            <ImageBackground source = {require("../assets/images/Background.jpg")} resizeMode="repeat" style={styles.image}>
-                <View style={{ display: "flex", justifyContent:"center", marginTop: 20}}>
-                    <Card mode="outlined" style={{width:"80vw", height:"60vh", display:"flex", alignItems:"center"}}>
-                        <Card.Title title="Utilisateur" left={LeftContent}/>
-                        <Card.Content>
-                        <Title>Mathieu ALBIN</Title>
-                        <Paragraph>mathiiew972@gmail.com</Paragraph>
-                        <Paragraph>0695342355</Paragraph>
-                        </Card.Content>
-                        <Card.Actions>
-                            <Button textColor="green" mode="outlined">Modifier</Button>
-                        </Card.Actions>
-                        <Card.Content>
-                            <Paragraph>
-                                Change Password
-                                <TextInput mode="outlined" activeOutlineColor="green" label="password" style={{ margin: 5 }} />
-                                <TextInput mode="outlined" activeOutlineColor="green" label="confirmation" style={{ margin: 5 }} />
-                            </Paragraph>
-                        <Card.Actions>
-                            <Button textColor="green" mode="outlined">Valider</Button>
-                        </Card.Actions>
-                        </Card.Content>
-                    </Card>
+                <View style={{ display: "flex", justifyContent:"center", alignItems:"center", marginTop: 20}}>
+                    <Text darkColor="black">Mathieu ALBIN</Text>
+                    <Paragraph>mathiiew972@gmail.com</Paragraph>
+                    <Paragraph>0695342355</Paragraph>
+                    <Button textColor="green" mode="outlined">Modifier</Button>
+                </View>
+                <View>
+                    <Paragraph>
+                        Change Password
+                        <TextInput mode="outlined" activeOutlineColor="green" label="password" style={{ margin: 5 }} />
+                        <TextInput mode="outlined" activeOutlineColor="green" label="confirmation" style={{ margin: 5 }} />
+                    </Paragraph>
+                    <Button textColor="green" mode="outlined">Valider</Button>
                 </View>
            </ImageBackground>
         </View>
