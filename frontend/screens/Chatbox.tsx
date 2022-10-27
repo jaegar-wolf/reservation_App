a52abfe37e01e03bffb03f90e229dc8622182031
Amayes-ExR <amayes.salhi@exxact-robotics.com>
2022-10-27 19:02:56 +0200

    Nous contacter added

--- frontend/screens/Chatbox.tsx
+++ frontend/screens/Chatbox.tsx
@@ -1,7 +1,7 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet,Text } from 'react-native';
 import * as React from 'react';
 import { RootStackScreenProps } from '../types';
-import { TextInput } from 'react-native-paper';
+import { Avatar,TextInput, Button} from 'react-native-paper';
 import { View } from '../components/Themed';
 
 export default function Chatbox({ navigation }: RootStackScreenProps<'Chatbox'>) {
@@ -11,42 +11,70 @@ export default function Chatbox({ navigation }: RootStackScreenProps<'Chatbox'>)
   const [email, setemail] = React.useState("");
 
   return (
-    <View style={{backgroundColor:'black',alignItems:'center'}}>
-<TextInput
-      label="Nom"
+    <View style={{flexGrow: 1,backgroundColor:'black', display:'flex', alignItems:'center'}}>
       
-      value={nom}
-      mode="flat"
-      style={{marginTop:80,width:'50%'}}
-      onChangeText={nom => setnom(nom)}
-    />
+      <TextInput
+            label="Nom"
+            value={nom}
+            mode="outlined"
+            style={{marginTop:30,height:'5vh'}}
+            onChangeText={nom => setnom(nom)}
+          />
 
-<TextInput
-      label="Prénom"
-      value={prenom}
-      mode="flat"
-      style={{marginTop:90,height:'5vh'}}
-      onChangeText={prenom => setprenom(prenom)}
-    />
-<TextInput
-      label="Email"
-      value={email}
-      mode="flat"
-      style={{marginTop:100,height:'5vh'}}
-      onChangeText={email => setemail(email)}
-    />    
-
-<TextInput
-      multiline={true}
-      numberOfLines={10}
-      label="Email"
-      value={email}
-      mode="flat"
-      style={{marginTop:100,height:'30vh'}}
-      onChangeText={email => setemail(email)}
-    />  
+      <TextInput
+            label="Prénom"
+            value={prenom}
+            mode="outlined"
+            style={{marginTop:30,height:'5vh'}}
+            onChangeText={prenom => setprenom(prenom)}
+          />
+      <TextInput
+            label="Email"
+            value={email}
+            mode="outlined"
+            style={{marginTop:30,height:'5vh'}}
+            onChangeText={email => setemail(email)}
+          />   
+      <TextInput
+            label="Message"
+            value={text}
+            multiline = {true}
+            numberOfLines={10}
+            mode="outlined"
+            style={{marginTop:30,height:'15vh'}}
+            onChangeText={text => setText(text)}
+          />                   
+      <Button style = {styles.button} onPress={() => navigation.navigate("Login")}>
+          <Text style={styles.text}>Envoyez</Text>
+        </Button>
+  
 
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+
+  button: {
+    alignItems: 'center',
+    marginBottom:10,
+    paddingHorizontal: 1,
+    borderRadius: 3,
+    marginTop:15,
+    elevation: 1,
+    backgroundColor: 'black',
+  },
+
+
+  text: {
+    color: "#bc6c25",
+    fontSize: 10,
+    width:"90%",
+    marginTop:3,
+    fontFamily:'San-Franisco',
+    fontWeight:'bold',
+
+  },
+  
+});
 
