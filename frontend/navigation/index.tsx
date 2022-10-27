63741827caf9c913d996762efd0fe01f15a0a4e2
jaegar-wolf <mathiiew972@gmail.com>
2022-10-27 17:28:35 +0200

    quelques modif

--- frontend/navigation/index.tsx
+++ frontend/navigation/index.tsx
@@ -83,10 +83,12 @@ function BottomTabNavigator() {
   const colorScheme = useColorScheme();
 
   return (
-    <BottomTab.Navigator
+    <BottomTab.Navigator 
       initialRouteName="Main"
       screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme].tint,
+        tabBarActiveTintColor: "#fdb833",
+        tabBarActiveBackgroundColor:"black",
+        tabBarInactiveBackgroundColor:"black",
       }}>
       <BottomTab.Screen
         name="Main"
@@ -94,7 +96,7 @@ function BottomTabNavigator() {
         options={{
           title: 'Accueil',
           headerShown:false,
-          tabBarIcon: ({ color="green" }) => <FontAwesomeIcon icon={faHouse} color={color}/>,
+          tabBarIcon: ({ color }) => <FontAwesomeIcon icon={faHouse} color={color}/>,
         }}
       />
       <BottomTab.Screen
