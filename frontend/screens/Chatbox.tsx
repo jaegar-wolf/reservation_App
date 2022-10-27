4c5d40bc9561151c10f95f6db5cbf59b7ff9c4b9
jaegar-wolf <mathiiew972@gmail.com>
2022-10-27 19:52:55 +0200

    modif chatBox

--- frontend/screens/Chatbox.tsx
+++ frontend/screens/Chatbox.tsx
@@ -6,9 +6,9 @@ import { View } from '../components/Themed';
 
 export default function Chatbox({ navigation }: RootStackScreenProps<'Chatbox'>) {
   const [text, setText] = React.useState("");
-  const [nom, setnom] = React.useState("");
-  const [prenom, setprenom] = React.useState("");
-  const [email, setemail] = React.useState("");
+  const [nom, setNom] = React.useState("");
+  const [prenom, setPrenom] = React.useState("");
+  const [email, setEmail] = React.useState("");
 
   return (
     <View style={{flexGrow: 1,backgroundColor:'black', display:'flex', alignItems:'center'}}>
@@ -17,23 +17,23 @@ export default function Chatbox({ navigation }: RootStackScreenProps<'Chatbox'>)
             label="Nom"
             value={nom}
             mode="outlined"
-            style={{marginTop:30,height:'5vh'}}
-            onChangeText={nom => setnom(nom)}
+            style={{marginTop:30,height:50}}
+            onChangeText={nom => setNom(nom)}
           />
 
       <TextInput
             label="Prénom"
             value={prenom}
             mode="outlined"
-            style={{marginTop:30,height:'5vh'}}
-            onChangeText={prenom => setprenom(prenom)}
+            style={{marginTop:30,height:50}}
+            onChangeText={prenom => setPrenom(prenom)}
           />
       <TextInput
             label="Email"
             value={email}
             mode="outlined"
-            style={{marginTop:30,height:'5vh'}}
-            onChangeText={email => setemail(email)}
+            style={{marginTop:30,height:50}}
+            onChangeText={email => setEmail(email)}
           />   
       <TextInput
             label="Message"
@@ -41,10 +41,10 @@ export default function Chatbox({ navigation }: RootStackScreenProps<'Chatbox'>)
             multiline = {true}
             numberOfLines={10}
             mode="outlined"
-            style={{marginTop:30,height:'15vh'}}
+            style={{marginTop:30,height:160}}
             onChangeText={text => setText(text)}
           />                   
-      <Button style = {styles.button} onPress={() => navigation.navigate("Login")}>
+      <Button mode='outlined' style={{marginTop: 15}}>
           <Text style={styles.text}>Envoyez</Text>
         </Button>
   
@@ -55,20 +55,10 @@ export default function Chatbox({ navigation }: RootStackScreenProps<'Chatbox'>)
 
 const styles = StyleSheet.create({
 
-  button: {
-    alignItems: 'center',
-    marginBottom:10,
-    paddingHorizontal: 1,
-    borderRadius: 3,
-    marginTop:15,
-    elevation: 1,
-    backgroundColor: 'black',
-  },
-
 
   text: {
     color: "#bc6c25",
-    fontSize: 10,
+    fontSize: 16,
     width:"90%",
     marginTop:3,
     fontFamily:'San-Franisco',
